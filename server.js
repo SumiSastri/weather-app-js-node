d100b8016fcb2c96a4060322647a87a7561be7b9
Your Name <sumisastri@gmail.com>
2020-02-04 12:08:19 +0000

    debugs dotenv set up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config;
 }
-const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+const APIKEYNAME_API_KEY = process.env.APIKEYNAME_API_KEY;
 
 app.use(express.json());
 app.use(express.static('public'));
