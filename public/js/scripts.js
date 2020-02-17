c147bb02590056759949324456bbfaea60a6fb00
Your Name <sumisastri@gmail.com>
2020-02-17 21:48:10 +0000

    refactors - adds math round

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,19 @@
 console.log('javascript working');
+// const daysOfTheWeek = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
+// const monthsOfTheYear = [
+// 	'January',
+// 	'February,',
+// 	'March',
+// 	'April',
+// 	'May',
+// 	'June',
+// 	'July',
+// 	'Aug',
+// 	'Sept',
+// 	'Oct',
+// 	'Nov',
+// 	'Dec'
+// ];
 const searchLocation = document.querySelector('[data-city-search]');
 const searchBox = new google.maps.places.SearchBox(searchLocation);
 searchBox.addListener('places_changed', () => {
@@ -21,20 +36,28 @@ searchBox.addListener('places_changed', () => {
 		.then((res) => res.json())
 		.then((data) => {
 			setWeatherData(data, place.formatted_address);
+		})
+		.catch((err) => {
+			throw `Error: ${err}`;
 		});
 });
-const icon = new Skycons({ color: '#222' });
+const icon = new Skycons({ color: '' });
 const updateLocation = document.querySelector('[data-location]');
 const updateSummary = document.querySelector('[data-summary]');
 const updateTemperature = document.querySelector('[data-temperature]');
 const updatePrecipitation = document.querySelector('[data-precipitation]');
 const updateWind = document.querySelector('[data-wind]');
+// const timeStamp = new Date(data.currently.time * 1000);
+// const forecastDate = `${daysOfTheWeek[timeStamp.getDay()]} ${monthsOfTheYear[
+// 	timeStamp.getMonth()
+// ]} ${timeStamp.getDate()}}`;
+
 function setWeatherData(data, place) {
 	updateLocation.textContent = place;
 	updateSummary.textContent = `Summary: ${data.summary}`;
-	updateTemperature.textContent = `Temperature: ${data.temperature} degrees`;
-	updatePrecipitation.textContent = `Likelihood of rain: ${data.precipProbability * 100}%`;
-	updateWind.textContent = `Wind Speed (miles/km ph): ${data.windSpeed}`;
+	updateTemperature.textContent = `Temperature: ${Math.round(data.temperature)} degrees`;
+	updatePrecipitation.textContent = `Likelihood of rain: ${Math.round(data.precipProbability * 100)}%`;
+	updateWind.textContent = `Wind Speed (miles/km ph): ${Math.round(data.windSpeed)}`;
 	icon.set('icon', data.icon);
 	icon.play();
 }
